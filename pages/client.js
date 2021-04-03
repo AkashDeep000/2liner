7a02b8d121e5cf00ef6a428811295715843b1b82
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2021-04-03 10:34:48 +0530

    Create client.js

---
+++ pages/client.js
@@ -0,0 +1,16 @@
+import useSWR from 'swr'
+
+export default function Profile() {
+  const { status, error } = useSWR('/api/status', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (! status) return <div>loading...</div>
+  return   ( <ul>
+        {status.map((statu) => (
+          <li>
+            <h2>{statu.content}</h2>
+          
+          </li>)
+        ))}
+      </ul>
+}
