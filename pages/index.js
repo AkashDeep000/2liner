493c97d7c9babc1ba1825a69c6a9cb85d0543aa2
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2021-04-03 12:21:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,13 @@ export default function Home() {
       <Link href="/status/love">
           <a>love</a>
         </Link>
+        <br />
   <Link href="/db">
-          <a>DB</a>
+    <a>DB</a>
+          </Link>
+          <br />
+  <Link href="/client">
+          <a>Client</a>
         </Link>
     </>
   )
