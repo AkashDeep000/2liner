e871543a33b82e80cd4974631f1b45e9da79d5ac
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2021-04-05 17:19:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,24 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <a href="/imgplay-mod" download="ImgPlayMod>
-     <button styly={{
-  background-color:  -webkit-linear-gradient(35deg, rgba(245, 51, 150, 1), rgba(245, 125, 86, 1));
-  border: none;
-  color: white;
-  padding: 12px 30px;
-  cursor: pointer;
-  font-size: 20px;
-}}>Download</button>
+    <a href="/imgplay-mod" download="ImgPlayMod">
+     <button className="download">Download</button>
        </a>
       <br />
 <hr />
-  
+    
+        <br />
+  <Link href="/db">
+    <a>DB</a>
+          </Link>
           <br />
   <Link href="/client">
           <a>Client</a>
         </Link>
-<Link href="/time">
-    <a>Time</a>
-</Link>
+          <br />
+  <Link href="/time">
+          <a>Time</a>
+        </Link>
     </>
   )
 }
