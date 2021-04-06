b560da5177a36a5fb2dd8e740cceb99e0d810192
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2021-04-06 10:30:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-    <a href="https://f000.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_zde6d7912f6bd94647f830f19_f1007d34eaec0729d_d20210405_m175843_c000_v0001073_t0056" download="ImgPlayMod">
+    <a href="https://f000.backblazeb2.com/file/2liner/imgplay-mod.apk" download="ImgPlayMod">
      <button className="download">Download</button>
        </a>
       <br />
