b2d8fab191812c5f87ea9a423770ddb8623af3d8
AkashDeep000 <akashdeepdas970@gmail.com>
2021-05-05 00:13:36 +0530

    4666

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     register: true,
     scope: '/app',
-    sw: 'service-worker.js',
+    swSrc: 'service-worker.js',
   }
 })
 
---
+++ pages/_offline.js
@@ -0,0 +1,12 @@
+export default function Page({ data }) {
+ 
+  
+ 
+  return(
+    <>
+ <h3>No Internet Connection</h3>
+    </>
+    )
+}
+
+
