f9d7b5eb5c5a4cb8a539ab85674805e4112884c9
AkashDeep000 <akashdeepdas970@gmail.com>
2021-05-04 23:06:12 +0530

    ddgfs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+  }
+})
+
 module.exports = {
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
